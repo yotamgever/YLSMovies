338d6fc5c28e474ba675b8635889528121988848
LiorBentov <liorbentov@gmail.com>
2014-10-05 20:58:04 +0300

    Merge with Yotam

--- YLSMovies/MovieTheater/Scripts/movies.js
+++ YLSMovies/MovieTheater/Scripts/movies.js
@@ -60,7 +60,7 @@ function getMovieByID(ID) {
                             url: "Movie/getMovieStars",
                             type: "GET",
                             data: {
-                                "strMovieID" : ID
+                                "strMovieID": ID
                             },
                             success: function (data) {
                                 if (data != "-1") {
@@ -95,13 +95,18 @@ function addMovie() {
             "strName": selectedMovie.Title,
             "strDirector": selectedMovie.Director,
             "nYear": selectedMovie.Year * 1,
-            "strUserName" : "liorbentov"
+            "strUserName": "liorbentov"
         },
         success: function (answer) {
             if (answer == "True") {
-                $("#my-movies").append("<span class='col-md-2 savedMovie' onclick='getMovieByID(\"" + selectedMovie.imdbID
-                + "\")'>" + selectedMovie.Title + "</span>");
-                backToSearchResults();
+                $("#my-movies").append("<span class='col-md-2 savedMovie' id='" + selectedMovie.imdbID +
+                    "' onclick='getMovieByID(\"" + selectedMovie.imdbID
+                + "\")'>" + selectedMovie.Title +
+                "<button type='button' class='close' onclick='removeMovie(\"" + selectedMovie.imdbID +
+                "\");'><span aria-hidden='true'>&times;</span></button></span>");
+                debugger;
+                $("#myModal").modal('toggle');
+                $("a[href='#movies-my-movies']").click();
             }
         }
     });
@@ -118,7 +123,7 @@ function removeMovie(movieToRemove) {
             "strUserName": "liorbentov"
         },
         success: function (answer) {
-            if (answer == 'True'){
+            if (answer == true) {
                 $("#my-movies span[id='" + movieToRemove + "']").remove();
             }
         }
@@ -130,32 +135,75 @@ function backToSearchResults() {
     $("#movie-search-results").show();
 }
 
+function showMyMovies() {
+    $.ajax({
+        url: "Movie/getMyMovies",
+        type: "GET",
+        data: { "strUserName": "liorbentov" },
+        success: function (data) {
 
-$.ajax({
-    url: "Movie/getMyMovies",
-    type: "GET",
-    data: { "strUserName": "liorbentov" },
-    success: function (data) {
-        
-        for (i = 0; i < data.length; i++) {
-            $("#my-movies").append("<span class='col-md-2 savedMovie' id='" + data[i].IMDBID +
-                "' onclick='getMovieByID(\"" + data[i].IMDBID
-                + "\")'>" + data[i].Name + 
-                "<button type='button' class='close' onclick='removeMovie(\"" + data[i].IMDBID +
-                "\");'><span aria-hidden='true'>&times;</span></button></span>");
+            for (i = 0; i < data.length; i++) {
+                $("#my-movies").append("<span class='col-md-2 savedMovie' id='" + data[i].IMDBID +
+                    "' onclick='getMovieByID(\"" + data[i].IMDBID
+                    + "\")'>" + data[i].Name +
+                    "<button type='button' class='close' onclick='removeMovie(\"" + data[i].IMDBID +
+                    "\");'><span aria-hidden='true'>&times;</span></button></span>");
+            }
+
+            $("#my-movies button").click(function (event) {
+                event.stopPropagation();
+            });
         }
-
-        $("#my-movies button").click(function (event) {
-            event.stopPropagation();
-        });
-    }
-});
-
-function tryMe() {
-    alert("Hello");
+    });
 }
 
-$.ready = function () {
+function showTopRatedMovies() {
+    $.ajax({
+        url: "Movie/getTopRatedMovies",
+        type: "GET",
+        success: function (data) {
+            console.log(data);
+            if ($.fn.DataTable.isDataTable($('#top-rated'))) {
+                var oTable = $('#top-rated').dataTable();
+            }
+            else {
+                var oTable = $('#top-rated').dataTable({
+                    "autoWidth": false,
+                    "ordering": false,
+                    "columnDefs": [{
+                        "targets": [4],
+                        "mRender": function (data, type, full) {
+                            return "<a onClick=\"getMovieByID('" +
+                                data + "')\">See Details</a>";
+                        }
+                    }]
+                });
+            }
+            oTable.fnClearTable();
+
+            var json = [];
+            for (i = 0; i < data.length; i++) {
+                json.push([data[i].Name, data[i].Year, data[i].Director, data[i].Stars, data[i].IMDBID]);
+            }
+
+            oTable.fnAddData((json));
+        }
+    });
+
+}
+
+function Shtuty() {
+    $.ajax({
+        url: "Movie/getMostWatchesMovies",
+        type: "GET",
+        success: function (data) {
+            blablabla = data;
+        }
+    });
+}
+
+$(document).ready(function () {
+    // When the user clicks on any option other than Search-Results, the tab disappears
     $("a[role='tab']").each(function () {
         if ($(this).text() != 'Search Results')
             $(this).on("click", function () {
@@ -163,6 +211,11 @@ $.ready = function () {
             });
     });
 
+    $("a[href='#movies-top-rated']").on('click', function () {
+        showTopRatedMovies();
+    });
+
+    // Handle the stars rating
     $("input[type='radio']").on(
         'change', function () {
             $.ajax({
@@ -177,4 +230,50 @@ $.ready = function () {
                 }
             });
         });
+
+    showMyMovies();
+});
+
+var width = 960,
+    height = 500;
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var chart = d3.select("body")
+    .attr("width", width)
+    .attr("height", height);
+
+$.ajax({
+    url: "Movie/getTopRatedMovies",
+    type: "GET",
+    success: function (data) {
+        x.domain(data.map(function (d) { return d.Name; }));
+        y.domain([0, d3.max(data, function (d) { return d.Stars; })]);
+
+        var bar = chart.selectAll("g")
+            .data(data)
+          .enter().append("g")
+            .attr("transform", function (d) { return "translate(" + x(d.Name) + ",0)"; });
+
+        bar.append("rect")
+            .attr("y", function (d) { return y(d.Stars); })
+            .attr("height", function (d) { return height - y(d.Stars); })
+            .attr("width", x.rangeBand());
+
+        bar.append("text")
+            .attr("x", x.rangeBand() / 2)
+            .attr("y", function (d) { return y(d.Stars) + 3; })
+            .attr("dy", ".75em")
+            .text(function (d) { return d.Stars; });
+    }
+});
+
+function type(d) {
+    d.value = +d.value; // coerce to number
+    return d;
 }
+
